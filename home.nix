948685582f495978b8c422e755579f01e39922b2
Zain As If <zainasif154@gmail.com>
2026-02-10 05:35:05 +0000

    Hyprpaper initialised on startup

--- home.nix
+++ home.nix
@@ -1,30 +1,16 @@
 { config, pkgs, ... }:
 
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
   home.username = "zaina";
   home.homeDirectory = "/home/zaina";
-
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
   home.stateVersion = "26.05"; # Please read the comment before changing.
   
   # Enable gnome keyring
   services.gnome-keyring.enable = true;
   services.gnome-keyring.components = [ "secrets" "ssh" ];
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
+  # The home.packages option allows you to install Nix packages into your environment.
   home.packages = with pkgs; [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
     bash
     zsh
     neovim
@@ -64,33 +50,12 @@
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
     # # symlink to the Nix store copy.
     # ".screenrc".source = dotfiles/screenrc;
-    ".config/hypr".source = ./config/hypr;
 
     # # You can also set the file content immediately.
     # ".gradle/gradle.properties".text = ''
     #   org.gradle.console=verbose
     #   org.gradle.daemon.idletimeout=3600000
     # '';
-  };
-
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/zaina/etc/profile.d/hm-session-vars.sh
-  #
-  home.sessionVariables = {
-    # EDITOR = "emacs";
   };
 
   home.pointerCursor = {
@@ -135,27 +100,17 @@
 	"web-search"
       ];
     };
-
-    profileExtra = ''
-      if [ -z "$WAYLAND_DISPLAY" ] && [ "$XDG_VTNR" = 1 ]; then
-        exec uwsm start -S hyprland-uwsm.desktop
-      fi
-    '';
   };
 
   wayland.windowManager.hyprland = {
     enable = true;
-
-    extraConfig = ''
-      # temp 
-    '';
+    systemd.enable = false; # says no to home-manager editing, just want to link
   };
 
-  #xdg.configFile = {
-  #  # uncomment when doing neovim config
-  #  # nvim = ./config/nvim;
-  #  "hypr/hyprland.conf" = ./config/hypr/hyprland.conf;
-  #};
+  services.hyprpaper.enable = true;
+
+  # Creates Symlinks for directories or files
+  xdg.configFile."hypr".source = config.lib.file.mkOutOfStoreSymlink "/home/zaina/.dotfiles/config/hypr";
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
